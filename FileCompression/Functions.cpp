99ce3816772c948b069ae55fea8b64225675159a
Christian Fusco <christian.fusco@mymail.champlain.edu>
2017-04-06 12:32:43 -0400

    Updates Functions.cpp
    Replaced old functions with assembly versions.
    Added a function that returns whether a node is greater than another node.  Used in an std::sort.
    Random other updates.

--- FileCompression/Functions.cpp
+++ FileCompression/Functions.cpp
@@ -1,22 +1,6 @@
 #include "Header.h"
-
-/*
-Sometimes eof gets printed when it shouldn't be.  This prevents an output of 00000000,
-aka eof.  This should also prevent the printing of extra chars at the eof.
-
-PARA:
-a string of 1's and 0's that has a length of 7.
-RETURN:
-on success: the string with a 1 before it.
-on failure: returns zeros, which translates to a null char
-*/
-std::string addParityBit(std::string input)
-{
-	if (input.length() == 7)
-		return "1" + input;
-	std::cout << "parity err";
-	return "00000000";
-}
+#include <cstdio>
+#include <ctime>
 
 /*
 Converts a string from base 2 to base 10
@@ -25,16 +9,107 @@ a string of 1's and 0's
 RETURN:
 the base 10 equivalant of the binary string.
 */
-int convertStrToInt(std::string in)
-{
+int convertStrToInt(std::string in){
 	int num = 0;
-	for (int i = 0; i < in.length(); i++)
-	{
+	for (int i = 0; i < in.length(); i++){
 		char singleDigit = in[i];
-		if (singleDigit == '1')
-		{
+		if (singleDigit == '1'){
 			num += pow(2, (in.length() - i - 1));
 		}
 	}
 	return num;
+}
+
+/*
+Converts a string that contains a single character to a string
+that contains its ASCII value in 8 bits.
+PARA:
+An integer that represents a character value
+RETURN:
+The character's bit value as a string.
+*/
+std::string charToBitString(int c) {
+	std::string output = "        ";
+	_asm {
+		lea edi, output
+		mov eax, c
+		mov ebx, 128
+		mov ecx, 8
+		mov edx, 4
+		shiftAndAppend:
+		cmp al, 128
+			jb lessThan
+			mov bl, '1'
+			mov[edi + (edx)], bl
+			jmp end
+			lessThan :
+		mov bl, '0'
+			mov[edi + (edx)], bl
+			end :
+		inc edx
+			shl al, 1
+			loop shiftAndAppend
+	}
+	return output;
+}
+
+/*
+Converts a string that contains a single character to a string
+that contains its ASCII value in 8 bits.
+PARA:
+An integer that represents a character value
+RETURN:
+The character's bit value as a string.
+*/
+std::string charToBitStringSlow(int c) {
+	std::string toReturn = "";
+	while (c != 0) {
+		if (c % 2 == 0) {
+			toReturn = "0" + toReturn;
+		}
+		else {
+			toReturn = "1" + toReturn;
+		}
+		c /= 2;
+	}
+	//Prepend 0's if length is too small.
+	while (toReturn.length() < 8) {
+		toReturn = "0" + toReturn;
+	}
+	return toReturn;
+}
+
+/*
+Does a binary search on the vector to find target location
+PARA:
+A vector of nodes sorted by node.data
+A target double value, usually the data of the node to be inserted
+RETURN:
+The index of the node that was found
+*/
+int binarySearch(std::vector<Node*> nodeArr, double target) {
+	//find a midpoint
+	int min = 0;
+	int max = nodeArr.size();
+	while (min < max) {
+		int i = (min + max) / 2;
+		//double values will rarely be equal, so find a point where it just fits
+		if (nodeArr[i - 1]->data > target) {
+			if (nodeArr[i]->data <= target) {
+				return i;
+			}
+			else {
+				min = i + 1;
+			}
+		}
+		else {
+			max = i - 1;
+		}
+	}
+	return -1;
+}
+
+//Used for sorting with std::sort
+bool nodeDataGreaterThen(Node* left, Node* right) {
+	return left->data > right->data;
 }
